500eaa867f695bc66593d4f03aea410a8bb5cfe6
FarhanAgil <drakek1977@gmail.com>
2021-02-20 12:21:26 +0700

    Prak 3

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\PageController2;
+use App\Http\Controllers\ContactController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -36,8 +38,29 @@ use App\Http\Controllers\AboutController;
 // Route::get('/articles/{id}', [PageController::class, 'articles']);
 
 //Praktikum 2 no 3
-Route::get('/', [HomeController::class, 'welcome']);
+// Route::get('/', [HomeController::class, 'welcome']);
 
-Route::get('/about', [AboutController::class, 'about']);
+// Route::get('/about', [AboutController::class, 'about']);
 
-Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+// Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+// Praktikum 3
+Route::get('/', [PageController2::class, 'Index']);
+Route::prefix('/Product')->group(function () {
+    Route::get('/', [PageController2::class, 'Product']);
+    Route::get('/EduGames', [PageController2::class, 'EduGames']);
+    Route::get('/KidGames', [PageController2::class, 'KidGames']);
+    Route::get('/StoryBooks', [PageController2::class, 'StoryBooks']);
+    Route::get('/KidSongs', [PageController2::class, 'KidSongs']);    
+});
+Route::get('/News/{id}', [PageController2::class, 'News']);
+Route::prefix('/Program')->group(function () {
+    Route::get('/', [PageController2::class, 'Program']);
+    Route::get('/Karir', [PageController2::class, 'Karir']);
+    Route::get('/magang', [PageController2::class, 'magang']);
+    Route::get('/Kunjungan', [PageController2::class, 'Kunjungan']);    
+});
+Route::get('/AboutUs', [PageController2::class, 'AboutUs']);
+Route::resource('Contact', ContactController::class)->only ([
+    "index"
+]);
